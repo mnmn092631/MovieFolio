4ddefcae7b347df8efd065262ffb5d7e9b4e0c48
juhee kang <mnmn092631@gmail.com>
2024-02-14 02:17:59 +0900

    feat: 영화명으로 검색 가능하게 api 수정

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,6 +2,28 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest } from "next";
 
+interface MovieListAPIData {
+  movieListResult: {
+    movieList: {
+      movieCd: string;
+      movieNm: string;
+      movieNmEn: string;
+      prdtYear: string;
+      openDt: string;
+      genreAlt: string;
+      nationAlt: string;
+    }[];
+  };
+}
+interface MovieInfoAPIData {
+  movieInfoResult: {
+    movieInfo: {
+      showTm: string;
+      audits: { watchGradeNm?: string }[];
+    };
+  };
+}
+
 const prisma = new PrismaClient();
 
 export async function GET(req: NextApiRequest) {
@@ -13,6 +35,65 @@ export async function GET(req: NextApiRequest) {
   if (!keyword) return new NextResponse("", { status: 400 });
 
   try {
+    const findMovie: MovieListAPIData = await fetch(
+      `${process.env.MOVIE_LIST_BY_TITLE_API}${keyword}`,
+    ).then((data) => data.json());
+
+    if (findMovie.movieListResult.movieList.length === 0)
+      return new NextResponse("", { status: 404 });
+
+    const mappedDataPromises = findMovie.movieListResult.movieList.map(
+      async (item) => {
+        if (!item.movieCd) return;
+
+        const exData = await prisma.movie.findFirst({
+          where: {
+            id: item.movieCd,
+          },
+        });
+        if (exData) return;
+
+        const movieInfoData: MovieInfoAPIData = await fetch(
+          `${process.env.MOVIE_INFO_BY_ID_API}${item.movieCd}`,
+        ).then((data) => data.json());
+
+        return {
+          id: item.movieCd,
+          titleKo: item.movieNm,
+          titleEn: item.movieNmEn,
+          openYear: item.prdtYear,
+          openDate: item.openDt,
+          genre: item.genreAlt,
+          nation: item.nationAlt,
+          time: movieInfoData.movieInfoResult.movieInfo.showTm,
+          watchGradeNm:
+            movieInfoData.movieInfoResult.movieInfo.audits.length > 0
+              ? movieInfoData.movieInfoResult.movieInfo.audits[0].watchGradeNm
+              : "",
+        };
+      },
+    );
+
+    const mappedData = await Promise.all(mappedDataPromises);
+
+    for (const data of mappedData) {
+      if (!data) return;
+
+      await prisma.movie.create({
+        data: {
+          id: data.id,
+          titleKo: data.titleKo,
+          titleEn: data.titleEn,
+          openYear: data.openYear,
+          openDate: data.openDate,
+          genre: data.genre,
+          nation: data.nation,
+          time: data.time,
+          watchGradeNm: data.watchGradeNm || "",
+        },
+      });
+    }
+
     const keywordMovies = await prisma.movie.findMany({
       where: {
         OR: [
@@ -22,8 +103,6 @@ export async function GET(req: NextApiRequest) {
       },
     });
 
-    if (keywordMovies.length === 0)
-      return new NextResponse("", { status: 404 });
     return new NextResponse(JSON.stringify(keywordMovies), { status: 200 });
   } catch (err) {
     console.log(err);
