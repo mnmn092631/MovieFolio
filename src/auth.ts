761abd13303ad19ac8306cc6a6ecadc95dbaa13b
kang juhee <mnmn092631@gmail.com>
2024-02-20 14:35:54 +0900

    refactor: 불필요한 코드 정리

--- src/auth.ts
+++ src/auth.ts
@@ -6,53 +6,49 @@ import bcrypt from "bcryptjs";
 import prisma from "@/prisma";
 
 export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
+	handlers: { GET, POST },
+	auth,
+	signIn,
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  pages: {
-    signIn: "/login",
-    newUser: "/signup",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-    CredentialsProvider({
-      credentials: {
-        email: { label: "email", type: "email" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        if (!credentials?.email || !credentials?.password) return null;
+	adapter: PrismaAdapter(prisma),
+	pages: {
+		signIn: "/login",
+		newUser: "/signup",
+	},
+	session: {
+		strategy: "jwt",
+	},
+	providers: [
+		GoogleProvider({
+			clientId: process.env.AUTH_GOOGLE_ID,
+			clientSecret: process.env.AUTH_GOOGLE_SECRET,
+		}),
+		CredentialsProvider({
+			credentials: {
+				email: { label: "email", type: "email" },
+				password: { label: "password", type: "password" },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) return null;
 
-        console.log(credentials.email);
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email as string },
-        });
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email as string },
+				});
 
-        if (!user) return null;
+				if (!user) return null;
 
-        const passwordMatch = await bcrypt.compare(
-          credentials.password as string,
-          user.password!,
-        );
+				const passwordMatch = await bcrypt.compare(credentials.password as string, user.password!);
 
-        return passwordMatch ? user : null;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      return token;
-    },
-    async session({ session }) {
-      return session;
-    },
-  },
+				return passwordMatch ? user : null;
+			},
+		}),
+	],
+	callbacks: {
+		async jwt({ token }) {
+			return token;
+		},
+		async session({ session }) {
+			return session;
+		},
+	},
 });
