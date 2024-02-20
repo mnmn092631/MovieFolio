2c2243ab93cdcee7206085a359893e30baecdc53
kang juhee <mnmn092631@gmail.com>
2024-02-20 15:10:12 +0900

    refactor: signup typescript 관련 코드 정리

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,24 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import prisma from "@/prisma";
 
+interface SignupForm {
+	name: string;
+	email: string;
+	password: string;
+}
+
 export async function POST(request: NextRequest) {
 	try {
-		const body: { name: string; email: string; password: string } = await request.json();
+		const { name, email, password }: SignupForm = await request.json();
 
-		const user = await prisma.user.findUnique({
-			where: { email: body.email },
-		});
+		const user = await prisma.user.findUnique({ where: { email } });
 
 		if (user) return NextResponse.json({ message: "User with this email already exists" }, { status: 400 });
 
-		const hashedPassword = await bcrypt.hash(body.password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
 		const newUser = await prisma.user.create({
-			data: {
-				name: body.name,
-				email: body.email,
-				password: hashedPassword,
-			},
+			data: { name, email, password: hashedPassword },
 		});
 
 		return NextResponse.json({ email: newUser.email }, { status: 200 });
