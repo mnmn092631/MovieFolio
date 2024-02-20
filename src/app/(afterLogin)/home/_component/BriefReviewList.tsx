74ca77253384ebdb4c3ba4d4ccfafe42809d8121
juhee kang <mnmn092631@gmail.com>
2024-02-21 01:06:39 +0900

    fix: reviewList 인피니티 스크롤 문제 수정

--- src/app/(afterLogin)/home/_component/BriefReviewList.tsx
+++ src/app/(afterLogin)/home/_component/BriefReviewList.tsx
@@ -10,81 +10,91 @@ import { FaThumbsDown, FaThumbsUp } from "react-icons/fa";
 import styles from "./briefReviewList.module.scss";
 
 export default function BriefReviewList() {
-	const obsRef = useRef<HTMLDivElement>(null);
-	const preventRef = useRef<boolean>(true);
-	const endRef = useRef<boolean>(false);
-	const [page, setPage] = useState<number>(1);
+  const obsRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState<number>(1);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
 
-	const [reviews, setReviews] = useState<BriefReview[]>([]);
-	const [loading, setLoading] = useState(true);
-	const router = useRouter();
+  const [reviews, setReviews] = useState<BriefReview[]>([]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 });
-		if (obsRef.current) observer.observe(obsRef.current);
-		return () => observer.disconnect();
-	}, []);
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    if (!loading) {
+      observer = new IntersectionObserver(handleObserver, { threshold: 0.5 });
+      if (obsRef.current) observer.observe(obsRef.current);
+    }
 
-	useEffect(() => {
-		getBriefReviews();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [page]);
+    return () => observer && observer.disconnect();
+  }, [loading]);
 
-	const obsHandler = (entries: IntersectionObserverEntry[]) => {
-		const target = entries[0];
-		if (!endRef.current && target.isIntersecting && preventRef.current) {
-			preventRef.current = false;
-			setPage((prev) => prev + 1);
-		}
-	};
+  const handleObserver = (entries: IntersectionObserverEntry[]) => {
+    if (!isEnd && !loading && entries && entries[0].isIntersecting) loadMore();
+  };
 
-	const getBriefReviews = useCallback(async () => {
-		setLoading(true);
-		try {
-			const res = await fetch(`/api/review/brief?pageNo=${page}`);
-			if (res.ok) {
-				const data = await res.json();
-				if (data.isEnd) endRef.current = true;
-				setReviews((prev) => [...prev, ...data.list]);
-			}
-		} catch (err) {
-			console.log(err);
-		} finally {
-			setLoading(false);
-		}
-	}, [page]);
+  const getBriefReviews = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/review/brief?pageNo=${page}`);
+      if (res.ok) {
+        const data = await res.json();
+        if (data.isEnd) setIsEnd(true);
+        setReviews((prev) => prev.concat(data.list));
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [page]);
 
-	const onClick = (id: number) => router.push(`/home/brief?id=${id}`);
+  const loadMore = () => setPage((prev) => prev + 1);
 
-	if (loading) return <p className={styles.container}>...loading</p>;
-	if (!reviews.length) return <p className={styles.container}>No data.</p>;
-	return (
-		<div className={styles.container}>
-			{reviews.map((review) => (
-				<div key={review.id} className={styles.listCard} onClick={() => onClick(review.id)}>
-					<div className={styles.title}>
-						<h3>
-							{review.movie.titleKo}
-							<span>
-								<StarRating name={review.id} checkedValue={review.rating} isReadOnly={true} />
-								{review.rating}
-							</span>
-						</h3>
-						<span>{convertTimestampToSimple(review.createdAt)}</span>
-					</div>
-					<div className={styles.content}>
-						<div className={cx("pros", styles.pros)}>
-							<FaThumbsUp />
-							<p>{review.pros}</p>
-						</div>
-						<div className="cons">
-							<FaThumbsDown />
-							<p>{review.cons}</p>
-						</div>
-					</div>
-				</div>
-			))}
-			<div ref={obsRef}></div>
-		</div>
-	);
+  useEffect(() => {
+    if (loading) return;
+    getBriefReviews();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const onClick = (id: number) => router.push(`/home/brief?id=${id}`);
+
+  // if (loading) return <p className={styles.container}>...loading</p>;
+  if (!reviews.length) return <p className={styles.container}>No data.</p>;
+  return (
+    <div className={styles.container}>
+      {reviews.map((review) => (
+        <div
+          key={review.id}
+          className={styles.listCard}
+          onClick={() => onClick(review.id)}
+        >
+          <div className={styles.title}>
+            <h3>
+              {review.movie.titleKo}
+              <span>
+                <StarRating
+                  name={review.id}
+                  checkedValue={review.rating}
+                  isReadOnly={true}
+                />
+                {review.rating}
+              </span>
+            </h3>
+            <span>{convertTimestampToSimple(review.createdAt)}</span>
+          </div>
+          <div className={styles.content}>
+            <div className={cx("pros", styles.pros)}>
+              <FaThumbsUp />
+              <p>{review.pros}</p>
+            </div>
+            <div className="cons">
+              <FaThumbsDown />
+              <p>{review.cons}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div ref={obsRef}></div>
+    </div>
+  );
 }
--- src/app/(afterLogin)/home/_component/DetailedReviewList.tsx
+++ src/app/(afterLogin)/home/_component/DetailedReviewList.tsx
@@ -8,86 +8,96 @@ import convertTimestampToSimple from "@/app/(afterLogin)/_lib/convertTimestampTo
 import styles from "./detailedReviewList.module.scss";
 
 export default function DetailedReviewList() {
-	const obsRef = useRef<HTMLDivElement>(null);
-	const preventRef = useRef<boolean>(true);
-	const endRef = useRef<boolean>(false);
-	const [page, setPage] = useState<number>(1);
+  const obsRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState<number>(1);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
 
-	const [reviews, setReviews] = useState<DetailedReview[]>([]);
-	const [loading, setLoading] = useState(true);
-	const router = useRouter();
+  const [reviews, setReviews] = useState<DetailedReview[]>([]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 });
-		if (obsRef.current) observer.observe(obsRef.current);
-		return () => observer.disconnect();
-	}, []);
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    if (!loading) {
+      observer = new IntersectionObserver(handleObserver, { threshold: 0.5 });
+      if (obsRef.current) observer.observe(obsRef.current);
+    }
 
-	useEffect(() => {
-		getDetailedReviews();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [page]);
+    return () => observer && observer.disconnect();
+  }, [loading]);
 
-	const obsHandler = (entries: IntersectionObserverEntry[]) => {
-		const target = entries[0];
-		if (!endRef.current && target.isIntersecting && preventRef.current) {
-			preventRef.current = false;
-			setPage((prev) => prev + 1);
-		}
-	};
+  const handleObserver = (entries: IntersectionObserverEntry[]) => {
+    if (!isEnd && !loading && entries && entries[0].isIntersecting) loadMore();
+  };
 
-	const getDetailedReviews = useCallback(async () => {
-		setLoading(true);
-		try {
-			const res = await fetch(`/api/review/detailed?pageNo=${page}`);
-			if (res.ok) {
-				const data = await res.json();
-				if (data.isEnd) endRef.current = true;
-				setReviews((prev) => [...prev, ...data.list]);
-			}
-		} catch (err) {
-			console.log(err);
-		} finally {
-			setLoading(false);
-		}
-	}, [page]);
+  const getDetailedReviews = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/review/detailed?pageNo=${page}`);
+      if (res.ok) {
+        const data = await res.json();
+        if (data.isEnd) setIsEnd(true);
+        setReviews((prev) => prev.concat(data.list));
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [page]);
 
-	const onClick = (id: number) => router.push(`/home/detailed?id=${id}`);
+  const loadMore = () => setPage((prev) => prev + 1);
 
-	if (loading) return <p className={styles.container}>...loading</p>;
-	if (!reviews.length) return <p className={styles.container}>No data.</p>;
-	return (
-		<div className={styles.container}>
-			{reviews.map((review) => (
-				<div key={review.id} className={styles.listCard} onClick={() => onClick(review.id)}>
-					<h4 className={styles.movieNameRating}>
-						{review.movie.titleKo}
-						<span>
-							<StarRating name={review.id} checkedValue={review.rating} isReadOnly={true} />
-							<strong>{review.rating}</strong>
-						</span>
-					</h4>
-					<div className={styles.title}>
-						<h2>{review.title}</h2>
-						<span>{convertTimestampToSimple(review.createdAt)}</span>
-					</div>
-					<div className={styles.content}>
-						<p>
-							<strong>quotes</strong>
-							<span>{review.quotes}</span>
-						</p>
-						<p>
-							<strong>storyline</strong>
-							<span>{review.storyline}</span>
-						</p>
-						<p>
-							<strong>review</strong>
-							<span>{review.review}</span>
-						</p>
-					</div>
-				</div>
-			))}
-			<div ref={obsRef}></div>
-		</div>
-	);
+  useEffect(() => {
+    if (loading) return;
+    getDetailedReviews();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const onClick = (id: number) => router.push(`/home/detailed?id=${id}`);
+
+  // if (loading) return <p className={styles.container}>...loading</p>;
+  if (!reviews.length) return <p className={styles.container}>No data.</p>;
+  return (
+    <div className={styles.container}>
+      {reviews.map((review) => (
+        <div
+          key={review.id}
+          className={styles.listCard}
+          onClick={() => onClick(review.id)}
+        >
+          <h4 className={styles.movieNameRating}>
+            {review.movie.titleKo}
+            <span>
+              <StarRating
+                name={review.id}
+                checkedValue={review.rating}
+                isReadOnly={true}
+              />
+              <strong>{review.rating}</strong>
+            </span>
+          </h4>
+          <div className={styles.title}>
+            <h2>{review.title}</h2>
+            <span>{convertTimestampToSimple(review.createdAt)}</span>
+          </div>
+          <div className={styles.content}>
+            <p>
+              <strong>quotes</strong>
+              <span>{review.quotes}</span>
+            </p>
+            <p>
+              <strong>storyline</strong>
+              <span>{review.storyline}</span>
+            </p>
+            <p>
+              <strong>review</strong>
+              <span>{review.review}</span>
+            </p>
+          </div>
+        </div>
+      ))}
+      <div ref={obsRef}></div>
+    </div>
+  );
 }
