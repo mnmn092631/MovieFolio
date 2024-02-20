a998c48b7439e981129a72d8dd03cfd7421d0925
juhee kang <mnmn092631@gmail.com>
2024-02-21 00:57:32 +0900

    fix: router 잘못 import한 것 수정

--- src/app/(afterLogin)/_component/BackButton.tsx
+++ src/app/(afterLogin)/_component/BackButton.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { IoIosArrowBack } from "react-icons/io";
 import styles from "./backButton.module.scss";
 
 export default function BackButton() {
-	const router = useRouter();
-	const onClickBack = () => router.back();
+  const router = useRouter();
+  const onClickBack = () => router.back();
 
-	return (
-		<button className={styles.backButton} onClick={onClickBack}>
-			<IoIosArrowBack />
-			back
-		</button>
-	);
+  return (
+    <button className={styles.backButton} onClick={onClickBack}>
+      <IoIosArrowBack />
+      back
+    </button>
+  );
 }
