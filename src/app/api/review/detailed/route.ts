fbfce1984bd5b687e8eca20bc4dbbec846dfdb18
kang juhee <mnmn092631@gmail.com>
2024-02-20 10:03:11 +0900

    feat: 리뷰 리스트 및 영화 검색 리스트 api pagination

--- src/app/api/review/detailed/route.ts
+++ src/app/api/review/detailed/route.ts
@@ -4,129 +4,136 @@ import prisma from "@/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req?: NextRequest) {
-  const session = await auth();
-  // todo: status 확인
-  if (!session?.user?.email) return new NextResponse("", { status: 400 });
+	const session = await auth();
+	// todo: status 확인
+	if (!session?.user?.email) return new NextResponse("", { status: 400 });
 
-  const searchParams = req?.nextUrl.searchParams;
-  const id = searchParams?.get("id");
+	const searchParams = req?.nextUrl.searchParams;
+	const id = searchParams?.get("id");
 
-  if (!id) {
-    const detailedReviews = await prisma.detailedReview.findMany({
-      where: { author: { email: session.user.email } },
-      include: {
-        movie: true,
-      },
-    });
+	if (!id) {
+		const perPage = 10;
+		const pageNo = Number(searchParams?.get("pageNo")) ?? 1;
 
-    return new NextResponse(JSON.stringify(detailedReviews), { status: 200 });
-  }
+		const detailedReviews = await prisma.detailedReview.findMany({
+			where: { author: { email: session.user.email } },
+			include: {
+				movie: true,
+			},
+		});
 
-  if (!isNaN(Number(id))) {
-    const detailedReview = await prisma.detailedReview.findUnique({
-      where: { id: Number(id) },
-      include: {
-        movie: true,
-      },
-    });
+		const totalCount = detailedReviews.length;
+		const totalPage = Math.ceil(totalCount / perPage);
+		if (pageNo > totalPage) return new NextResponse("", { status: 400 });
 
-    return new NextResponse(JSON.stringify(detailedReview), { status: 200 });
-  }
+		let pagination = detailedReviews;
+		if (totalCount > perPage) {
+			if (pageNo === totalPage) pagination = detailedReviews.slice(perPage * (pageNo - 1), totalCount);
+			else pagination = detailedReviews.slice(perPage * (pageNo - 1), perPage * pageNo);
+		}
 
-  return new NextResponse("", { status: 404 });
+		return new NextResponse(JSON.stringify(pagination), { status: 200 });
+	}
+
+	if (!isNaN(Number(id))) {
+		const detailedReview = await prisma.detailedReview.findUnique({
+			where: { id: Number(id) },
+			include: {
+				movie: true,
+			},
+		});
+
+		return new NextResponse(JSON.stringify(detailedReview), { status: 200 });
+	}
+
+	return new NextResponse("", { status: 404 });
 }
 
 export async function POST(req: NextRequest) {
-  const session = await auth();
-  // todo: status 확인
-  if (!session?.user?.email) return new NextResponse("", { status: 400 });
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session.user.email,
-    },
-  });
-  if (!user) return new NextResponse("", { status: 400 });
+	const session = await auth();
+	// todo: status 확인
+	if (!session?.user?.email) return new NextResponse("", { status: 400 });
+	const user = await prisma.user.findUnique({
+		where: {
+			email: session.user.email,
+		},
+	});
+	if (!user) return new NextResponse("", { status: 400 });
 
-  const {
-    title,
-    watchedAt,
-    place,
-    rating,
-    storyline,
-    quotes,
-    review,
-    movieId,
-  }: Omit<
-    DetailedReview,
-    "id" | "createdAt" | "author" | "authorId" | "movie"
-  > = await req.json();
+	const {
+		title,
+		watchedAt,
+		place,
+		rating,
+		storyline,
+		quotes,
+		review,
+		movieId,
+	}: Omit<DetailedReview, "id" | "createdAt" | "author" | "authorId" | "movie"> = await req.json();
 
-  try {
-    await prisma.detailedReview.create({
-      data: {
-        title,
-        watchedAt,
-        place,
-        rating,
-        storyline,
-        quotes,
-        review,
-        authorId: user.id,
-        movieId,
-      },
-    });
-    return new NextResponse("", { status: 200 });
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("", { status: 500 });
-  }
+	try {
+		await prisma.detailedReview.create({
+			data: {
+				title,
+				watchedAt,
+				place,
+				rating,
+				storyline,
+				quotes,
+				review,
+				authorId: user.id,
+				movieId,
+			},
+		});
+		return new NextResponse("", { status: 200 });
+	} catch (err) {
+		console.log(err);
+		return new NextResponse("", { status: 500 });
+	}
 }
 
 export async function PUT(req: NextRequest) {
-  const {
-    id,
-    title,
-    watchedAt,
-    place,
-    rating,
-    storyline,
-    quotes,
-    review,
-  }: Omit<
-    DetailedReview,
-    "createdAt" | "author" | "authorId" | "movieId" | "movie"
-  > = await req.json();
+	const {
+		id,
+		title,
+		watchedAt,
+		place,
+		rating,
+		storyline,
+		quotes,
+		review,
+	}: Omit<DetailedReview, "createdAt" | "author" | "authorId" | "movieId" | "movie"> = await req.json();
 
-  try {
-    await prisma.detailedReview.update({
-      where: { id },
-      data: {
-        title,
-        watchedAt,
-        place,
-        rating,
-        storyline,
-        quotes,
-        review,
-      },
-    });
-    return new NextResponse("", { status: 200 });
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("", { status: 500 });
-  }
+	try {
+		await prisma.detailedReview.update({
+			where: { id },
+			data: {
+				title,
+				watchedAt,
+				place,
+				rating,
+				storyline,
+				quotes,
+				review,
+			},
+		});
+		return new NextResponse("", { status: 200 });
+	} catch (err) {
+		console.log(err);
+		return new NextResponse("", { status: 500 });
+	}
 }
 
 export async function DELETE(req: NextRequest) {
-  const { id }: { id: number } = await req.json();
+	const { id }: { id: number } = await req.json();
 
-  try {
-    await prisma.detailedReview.delete({
-      where: { id },
-    });
-    return new NextResponse("", { status: 200 });
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("", { status: 500 });
-  }
+	try {
+		await prisma.detailedReview.delete({
+			where: { id },
+		});
+		return new NextResponse("", { status: 200 });
+	} catch (err) {
+		console.log(err);
+		return new NextResponse("", { status: 500 });
+	}
 }
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,105 +2,100 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma";
 
 interface MovieListAPIData {
-  movieListResult: {
-    movieList: {
-      movieCd: string;
-      movieNm: string;
-      movieNmEn: string;
-      prdtYear: string;
-      openDt: string;
-      genreAlt: string;
-      nationAlt: string;
-    }[];
-  };
+	movieListResult: {
+		movieList: {
+			movieCd: string;
+			movieNm: string;
+			movieNmEn: string;
+			prdtYear: string;
+			openDt: string;
+			genreAlt: string;
+			nationAlt: string;
+		}[];
+	};
 }
 interface MovieInfoAPIData {
-  movieInfoResult: {
-    movieInfo: {
-      showTm: string;
-      audits: { watchGradeNm?: string }[];
-    };
-  };
+	movieInfoResult: {
+		movieInfo: {
+			showTm: string;
+			audits: { watchGradeNm?: string }[];
+		};
+	};
 }
 
 export async function GET(req: NextRequest) {
-  const searchParams = req.nextUrl.searchParams;
-  const keyword = searchParams.get("keyword");
+	const searchParams = req.nextUrl.searchParams;
+	const keyword = searchParams.get("keyword");
+	const pageNo = Number(searchParams?.get("pageNo")) ?? 1;
 
-  if (!keyword) return new NextResponse("", { status: 400 });
+	if (!keyword) return new NextResponse("", { status: 400 });
 
-  try {
-    const findMovie: MovieListAPIData = await fetch(
-      `${process.env.MOVIE_LIST_BY_TITLE_API}${keyword}`,
-    ).then((data) => data.json());
+	try {
+		const findMovie: MovieListAPIData = await fetch(
+			`${process.env.MOVIE_LIST_BY_TITLE_API}${keyword}&curPage=${pageNo}`
+		).then((data) => data.json());
 
-    if (findMovie.movieListResult.movieList.length === 0)
-      return new NextResponse("", { status: 404 });
+		if (findMovie.movieListResult.movieList.length === 0) return new NextResponse("", { status: 404 });
 
-    const mappedDataPromises = findMovie.movieListResult.movieList.map(
-      async (item) => {
-        const exData = await prisma.movie.findFirst({
-          where: {
-            id: item.movieCd,
-          },
-        });
-        if (exData) return;
+		const mappedDataPromises = findMovie.movieListResult.movieList.map(async (item) => {
+			const exData = await prisma.movie.findFirst({
+				where: {
+					id: item.movieCd,
+				},
+			});
+			if (exData) return;
 
-        const movieInfoData: MovieInfoAPIData = await fetch(
-          `${process.env.MOVIE_INFO_BY_ID_API}${item.movieCd}`,
-        ).then((data) => data.json());
+			const movieInfoData: MovieInfoAPIData = await fetch(`${process.env.MOVIE_INFO_BY_ID_API}${item.movieCd}`).then(
+				(data) => data.json()
+			);
 
-        return {
-          id: item.movieCd,
-          titleKo: item.movieNm,
-          titleEn: item.movieNmEn,
-          openYear: item.prdtYear,
-          openDate: item.openDt,
-          genre: item.genreAlt,
-          nation: item.nationAlt,
-          time: movieInfoData.movieInfoResult.movieInfo.showTm,
-          watchGradeNm:
-            movieInfoData.movieInfoResult.movieInfo.audits.length > 0
-              ? movieInfoData.movieInfoResult.movieInfo.audits[0].watchGradeNm
-              : "",
-        };
-      },
-    );
+			return {
+				id: item.movieCd,
+				titleKo: item.movieNm,
+				titleEn: item.movieNmEn,
+				openYear: item.prdtYear,
+				openDate: item.openDt,
+				genre: item.genreAlt,
+				nation: item.nationAlt,
+				time: movieInfoData.movieInfoResult.movieInfo.showTm,
+				watchGradeNm:
+					movieInfoData.movieInfoResult.movieInfo.audits.length > 0
+						? movieInfoData.movieInfoResult.movieInfo.audits[0].watchGradeNm
+						: "",
+			};
+		});
 
-    const mappedData = await Promise.all(mappedDataPromises);
+		const mappedData = await Promise.all(mappedDataPromises);
 
-    for (const data of mappedData) {
-      if (!data) continue;
+		for (const data of mappedData) {
+			if (!data) continue;
 
-      await prisma.movie.create({
-        data: {
-          id: data.id,
-          titleKo: data.titleKo,
-          titleEn: data.titleEn,
-          openYear: data.openYear,
-          openDate: data.openDate,
-          genre: data.genre,
-          nation: data.nation,
-          time: data.time,
-          watchGradeNm: data.watchGradeNm || "",
-        },
-      });
-    }
+			await prisma.movie.create({
+				data: {
+					id: data.id,
+					titleKo: data.titleKo,
+					titleEn: data.titleEn,
+					openYear: data.openYear,
+					openDate: data.openDate,
+					genre: data.genre,
+					nation: data.nation,
+					time: data.time,
+					watchGradeNm: data.watchGradeNm || "",
+				},
+			});
+		}
 
-    const keywordMovies = await prisma.movie.findMany({
-      where: {
-        OR: [
-          { titleKo: { contains: keyword } },
-          { titleEn: { contains: keyword } },
-        ],
-      },
-    });
+		const keywordMovies = await prisma.movie.findMany({
+			where: {
+				OR: [{ titleKo: { contains: keyword } }, { titleEn: { contains: keyword } }],
+			},
+		});
 
-    return new NextResponse(JSON.stringify(keywordMovies), { status: 200 });
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("", { status: 500 });
-  } finally {
-    await prisma.$disconnect();
-  }
+		return new NextResponse(JSON.stringify(keywordMovies), { status: 200 });
+	} catch (err) {
+		console.log(err);
+		return new NextResponse("", { status: 500 });
+	} finally {
+		await prisma.$disconnect();
+	}
 }
