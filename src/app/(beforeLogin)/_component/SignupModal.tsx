42cba44373ec13b157f8e79d4931d643073f0298
juhee kang <mnmn092631@gmail.com>
2024-02-21 22:44:41 +0900

    fix: 회원가입 모달 email label htmlFor 수정

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -6,59 +6,78 @@ import { RxCross2 } from "react-icons/rx";
 import styles from "./modal.module.scss";
 
 export default function SignupModal() {
-	const [form, setForm] = useState({
-		name: "",
-		email: "",
-		password: "",
-	});
-	const router = useRouter();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const router = useRouter();
 
-	const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-		e.preventDefault();
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
 
-		try {
-			const { name, email, password } = form;
-			const res = await fetch("/api/signup", {
-				method: "POST",
-				body: JSON.stringify({ name, email, password }),
-				credentials: "include",
-			});
+    try {
+      const { name, email, password } = form;
+      const res = await fetch("/api/signup", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+        credentials: "include",
+      });
 
-			if (res.ok) router.push("/login");
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	const onClickClose = () => router.back();
-	const onChange: ChangeEventHandler<HTMLInputElement> = (e) => setForm({ ...form, [e.target.id]: e.target.value });
+      if (res.ok) router.push("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const onClickClose = () => router.back();
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setForm({ ...form, [e.target.id]: e.target.value });
 
-	return (
-		<div className={styles.container}>
-			<div>
-				<button className={styles.closeButton} onClick={onClickClose}>
-					<RxCross2 />
-				</button>
-				<form onSubmit={onSubmit}>
-					<div className={styles.inputDiv}>
-						<label htmlFor="name">name</label>
-						<input type="text" id="name" value={form.name} onChange={onChange} />
-					</div>
+  return (
+    <div className={styles.container}>
+      <div>
+        <button className={styles.closeButton} onClick={onClickClose}>
+          <RxCross2 />
+        </button>
+        <form onSubmit={onSubmit}>
+          <div className={styles.inputDiv}>
+            <label htmlFor="name">name</label>
+            <input
+              type="text"
+              id="name"
+              value={form.name}
+              onChange={onChange}
+            />
+          </div>
 
-					<div className={styles.inputDiv}>
-						<label htmlFor="id">email</label>
-						<input type="email" id="email" value={form.email} onChange={onChange} />
-					</div>
+          <div className={styles.inputDiv}>
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              id="email"
+              value={form.email}
+              onChange={onChange}
+            />
+          </div>
 
-					<div className={styles.inputDiv}>
-						<label htmlFor="password">password</label>
-						<input type="password" id="password" value={form.password} onChange={onChange} />
-					</div>
+          <div className={styles.inputDiv}>
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={onChange}
+            />
+          </div>
 
-					<button className={styles.submitButton} disabled={!form.email || !form.name || !form.password}>
-						signup
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+          <button
+            className={styles.submitButton}
+            disabled={!form.email || !form.name || !form.password}
+          >
+            signup
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
